3242731a5a80d0720b509a1d6f56a2b0f939514d
Arman Yazdi <f41c0n.32@gmail.com>
2023-01-10 03:26:40 +0330

    Add providers.py

---
+++ emailtools/providers.py
@@ -0,0 +1,16 @@
+providers = {
+    'gmail': 'gmail.com',
+    'google': 'gmail.com',
+    'yahoo': 'yahoo.com',
+    'outlook': 'outlook.com',
+    'proton': 'proton.me',
+    'protonmail': 'proton.me',
+    'icloud': 'icloud.com',
+    'icloudmail': 'icloud.com',
+    'aol': 'aol.com',
+    'chapar': 'chmail.ir',
+    'chaapaar': 'chmail.ir',
+    'chmail': 'chmail.ir',
+    'mailfa': 'mailfa.com',
+    'mihanmail': 'mihanmail.ir',
+}
