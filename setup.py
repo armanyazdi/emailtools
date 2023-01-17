a91ce1f0b71eedaa95dd8061c80076b407a2921c
Arman Yazdi <f41c0n.32@gmail.com>
2023-01-17 11:21:42 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as f:
 
 setup(
     name='emailtools',
-    version='0.0.2',
+    version='0.0.3',
     packages=['emailtools'],
     include_package_data=True,
     url='https://github.com/armanyazdi/emailtools',
